2a1f9e6e4e35630677779b0a1175d03862b4d12a
byteworks <byteworks@brussels.(none)>
2010-11-23 20:29:45 -0600

    Finally got rot13 to work!

--- rot13_test.rb
+++ rot13_test.rb
@@ -27,17 +27,49 @@ class Rot13 < Test::Unit::TestCase
     # To change this template use File | Settings | File Templates.\
     assert_equal "o", rot13("b")
   end
+  def test_Q
+
+    # To change this template use File | Settings | File Templates.\
+    assert_equal "D", rot13("Q")
+  end
+
+  def test_n
+    assert_equal 'a', rot13('n')
+  end
+  def test_m
+    assert_equal 'z', rot13('m')
+  end
+
+  def test_N
+    assert_equal 'A', rot13('N')
+  end
 
   def test_ascii
     a='b'
     assert_equal 98, a[0].ord()
     assert_equal 'B', 66.chr
+    assert_equal "z", 122.chr
   end
 
   def rot13(n)
     #chip brains ruby line
-    ascii_value=n[0]
-    ascii_value+=13
-    return ascii_value.chr
+    ascii_value=n[0].ord()
+    #a=6
+    puts "before: " + ascii_value.to_s
+    if (ascii_value>=65 and ascii_value<=77)
+      ascii_value += 13
+    end
+    if (ascii_value>=78 and ascii_value<=90)
+      ascii_value -= 13
+    end
+    if (ascii_value>= 97 and ascii_value<=109)
+      ascii_value += 13
+    end
+    if (ascii_value>=110 and ascii_value<=122)
+      ascii_value -= 13
+    end
+    puts "after: " + ascii_value.to_s
+
+    return ascii_value.chr()
   end
 end
